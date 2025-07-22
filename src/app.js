bf43919ea0099e260ca8e4f6079c8a3594029567
Diego Brito <diego.brito@totvs.com.br>
2025-07-22 11:23:37 -0300

    fix: modicar o os dados da entidade para consulta no banco de dados

--- src/app.js
+++ src/app.js
@@ -1,35 +1,48 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./model/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão ", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexao com o banco feita com sucesso")
+});
 
 const app = express();
 app.use(express.json())
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos Anéis"
-    },{
-        id: 2,
-        titulo: "O Hobbit"
-    }
-];
+//const livros = [
+//    {
+//        id: 1,
+//        titulo: "O senhor dos Anéis"
+//    },{
+//        id: 2,
+//        titulo: "O Hobbit"
+//    }
+//];
 
-function buscarlivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
+//function buscarlivro(id) {
+//    return livros.findIndex(livro => {
+//        return livro.id === Number(id);
+//    })
+//}
 
 
 app.get("/", (req, res) => {
     res.status(200).send("curso de Node.js")
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros)
+app.get("/livros", async (req, res) => {
+    //const listaLivros = await livro.find({});
+    //res.status(200).json(listaLivros);
 });
 
 app.post("/livros", (req, res) => {
-    livros.push(req.body);
+    //livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso")
 });
 
