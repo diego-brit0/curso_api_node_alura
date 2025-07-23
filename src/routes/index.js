4d66d5fe04cf0f16bd33a1cdeae6d5ba58304ff6
Diego Brito <diego.brito@totvs.com.br>
2025-07-23 11:06:15 -0300

    feat: adicionar as rotas de autor ao express

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import livrosRt from "./livrosRoutes.js"
+import autoresRt from "./autorRoutes.js"
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
 
     app.use(express.json(), livrosRt)
+    app.use(express.json(), autoresRt)
 };
 
 export default routes;
