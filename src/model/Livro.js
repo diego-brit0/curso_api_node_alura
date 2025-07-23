fdbc11a33226a63e75c7f4203b8142079bf2e86b
Diego Brito <diego.brito@totvs.com.br>
2025-07-23 11:05:48 -0300

    feat: criar o relacionamenot no modelo de livro com autor

--- src/model/Livro.js
+++ src/model/Livro.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const LivroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
@@ -6,7 +7,8 @@ const LivroSchema = new mongoose.Schema({
     editora: { type: mongoose.Schema.Types.String},
     preco: { type: Number },
     paginas: { type: Number },
-    
+    autor: autorSchema
+    //autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true} 
 }, { versionKey: false } );
 
 const livro = mongoose.model("livros", LivroSchema);
