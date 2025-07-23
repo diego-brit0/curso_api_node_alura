7685afa8c468aad00456a982f12d2c81cc49eecf
Diego Brito <diego.brito@totvs.com.br>
2025-07-23 11:06:48 -0300

    feat: criar a rota de buscar livros por query

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,10 +4,14 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
+//rota de maior complexidade tem prioridade de ficar acima a rotas de menor complexidade
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/buscar", LivroController.listarLivrosPorEditora);
 routes.get("/livros/:id", LivroController.buscarLivroPorId);
+
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
 routes.delete("/livros/:id", LivroController.deletarPorId);
+
 
 export default routes;
